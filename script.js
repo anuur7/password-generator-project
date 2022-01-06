40f86d6f44d3b752c32fe17e88c229121f61ec44
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-06 15:12:40 +0000

    made a function for generate password and used an IF statement for the prompt if password is less than 8 char or more than 128 char and if password is an empty string or NaN

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const lowerCaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
 const upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const specialCharacters = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\" ]
 
+function generatePassword() {
+  var pw = prompt("Please choose a length of between 8 and 128 characters")
+  if (pw < 8 || pw > 128) {
+    alert("Incorrect password length")
+    generatePassword()
+  } else if (pw === '' || isNaN(pw)) {
+    alert("Please enter valid password between 8 and 128")
+    generatePassword()
+  }
+  
+
+}
 
 // Write password to the #password input
 function writePassword() {
