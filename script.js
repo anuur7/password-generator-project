1451118d913fa7fd4907e6fb91b1d8fb4d2f772e
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-09 19:45:59 +0000

    made an empty array so that all my choices are concatted into one array with an if statement

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const numbers = [1,2,3,4,5,6,7,8,9,0];
 const lowerCaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const specialCharacters = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\" ]
+let choices = []
 
 function generatePassword() {
   var pw = prompt("Please choose a length of between 8 and 128 characters")
@@ -15,10 +16,28 @@ function generatePassword() {
     generatePassword()
   }
 
-  var lowerCase = confirm("Would you like lower case in your password");
-  var upperCase = confirm("Would you like upper case in your password");
-  var numeric = confirm("Would you like a numeric in your password");
-  var special = confirm("Would you like special character in your password");
+  var lowerCase = confirm("Would you like lower case in your password?");
+  var upperCase = confirm("Would you like upper case in your password?");
+  var numeric = confirm("Would you like a numeric in your password?");
+  var special = confirm("Would you like special character in your password?");
+
+
+   if (lowerCase == true){
+    choices = choices.concat(lowerCaseAlphabet);
+  } 
+  if (upperCase == true){
+    choices = choices.concat(upperCaseAlphabet);
+  }
+  if (special == true){
+    choices = choices.concat(specialCharacters);
+  }
+  if (numeric == true){
+    choices = choices.concat(numbers);
+  } 
+  if (lowerCase == false && upperCase == false && special == false && numeric ==false){
+    alert('You need to select atleast one option')
+  }
+  console.log(choices)
 
 }
 
